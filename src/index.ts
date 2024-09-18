9e7f5599284d091c653b2574d91c81cf11da91ea
Benedikt Kulmann <benedikt@kulmann.biz>
2024-09-18 08:49:40 +0200

    feat: build most basic route and view

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import { defineWebApplication, ApplicationSetupOptions, Extension } from '@ownclouders/web-pkg'
+import {
+  defineWebApplication,
+  ApplicationSetupOptions,
+  Extension,
+  AppMenuItemExtension
+} from '@ownclouders/web-pkg'
+import { urlJoin } from '@ownclouders/web-client'
+import { RouteRecordRaw } from 'vue-router'
 import { computed } from 'vue'
 import { useGettext } from 'vue3-gettext'
 
@@ -7,18 +14,48 @@ export default defineWebApplication({
     const { $gettext } = useGettext()
 
     const appInfo = {
+      id: 'skeleton',
       name: $gettext('Skeleton'),
-      id: 'skeleton'
+      icon: 'cup',
+      color: '#622a0f'
     }
+
+    const routes: RouteRecordRaw[] = [
+      {
+        path: '/',
+        redirect: `/${appInfo.id}/hello`
+      },
+      {
+        path: '/hello',
+        name: 'hello',
+        component: () => import('./views/Hello.vue'),
+        meta: {
+          authContext: 'user',
+          title: $gettext('Hello')
+        }
+      }
+    ]
 
     const extensions = ({ applicationConfig }: ApplicationSetupOptions) => {
       return computed<Extension[]>(() => {
-        return []
+        const menuItems: AppMenuItemExtension[] = [
+          {
+            // registers a menu item for the app switcher
+            id: `app.${appInfo.id}.menuItem`,
+            type: 'appMenuItem',
+            label: () => appInfo.name,
+            color: appInfo.color,
+            icon: appInfo.icon,
+            path: urlJoin(appInfo.id)
+          }
+        ]
+        return [...menuItems]
       })
     }
 
     return {
       appInfo,
+      routes,
       extensions: extensions(args)
     }
   }
